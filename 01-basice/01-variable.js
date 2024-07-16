cc93165356e829858b848cfc8bb685c7e46012d6
Dimplemali8004 <113502480+Dimplemali8004@users.noreply.github.com>
2024-07-16 06:05:49 +0000

    variable code

---
+++ 01-basice/01-variable.js
@@ -0,0 +1,17 @@
+const accountId = 1234
+let accountEmail = "dimplemali80045@gamil.com"
+var accountPassword = "212121"
+accountCity = "jaipur"
+let accountState;
+
+//accountId = 2 or not allwed
+
+accountEmail = "mukeshgamil"
+accountPassword = "23456786"
+accountCity = "dehli"
+
+console.log(accountId);
+
+//perfer  not to use  var because of issue in block scope functional scope
+
+console.table([accountId, accountEmail, accountPassword, accountCity ,accountState])
