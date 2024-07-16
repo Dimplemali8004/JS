5eb6c044fcad6b614d6595d71faa1aa745690114
Dimplemali8004 <113502480+Dimplemali8004@users.noreply.github.com>
2024-07-16 07:38:09 +0000

    done

---
+++ 01-basice/datatyps-summary.js
@@ -0,0 +1,20 @@
+//primitive 
+
+// 7 types : String, Number, Boolearn, null, undefined, Symbol,BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 364797209890n
+
+//non primitive
+// object, function,array
