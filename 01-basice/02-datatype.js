e9f8df51b2c85aa5fb2bfe7ef8b213baecb560d4
Dimplemali8004 <113502480+Dimplemali8004@users.noreply.github.com>
2024-07-16 06:29:13 +0000

    datatype code

---
+++ 01-basice/02-datatype.js
@@ -0,0 +1,25 @@
+"use Strict"; //treat all JS code as newer vesion
+
+//alert(3 + 3) we are using nodejs, not browser
+
+console.log(3 
+    + 
+    3) //code readebility should be high
+
+    console.log("Dimple")
+
+    let name = "Dimple"
+    let age = "21"
+    let isLoggedIn = false
+
+    //number => 2 to power 53
+    //bigint
+    //String => ""
+    //boolean => true/false
+    //null => standalone value
+    //undefined =>
+    //symbol => unique
+    
+    //object
+
+    console.log(typeof "Dimple")
